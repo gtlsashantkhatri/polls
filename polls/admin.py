ab7b087b36dedb7b2f9e6df687cf7f37aec62e4d
Sashant Khatri <sashant.khatri@gatewaytechnolabs.com>
2018-08-29 17:14:27 +0530

    Add search and rename 'was_published_recently' in admin question view
    Add search on question in 'admin/polls/question' using search_fields attribute of ModelAdmin.
    
    Rename 'was_published_recently' to 'Published Recently' in 'admin/polls/question by adding
    short description in 'Question' model of poll apps.

--- polls/admin.py
+++ polls/admin.py
@@ -15,6 +15,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
 
 admin.site.register(Question,QuestionAdmin)
 #admin.site.register(Choice)
