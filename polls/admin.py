56c28e556ac185b8e14861da90249e3e5a580d9b
Sashant Khatri <sashant.khatri@gatewaytechnolabs.com>
2018-08-29 16:57:23 +0530

    Add TabularInline for Choice object
    Add TabularInline for Choice objects(ForiegnKey) and it shows in tabular form instead of
    stackedinline

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -16,4 +16,4 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice)
+#admin.site.register(Choice)
