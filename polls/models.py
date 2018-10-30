f343ed4deaffe7cd8e52e3abeb159c1da86ec5dc
Sashant Khatri <sashant.khatri@gatewaytechnolabs.com>
2018-10-30 11:22:58 +0530

    Update models.py for github and jenkins

--- polls/models.py
+++ polls/models.py
@@ -26,3 +26,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+#    class Meta:
+#        db_name = 'Person'
